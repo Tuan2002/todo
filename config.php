38267ad25c01f34efd994cc54999a3c2c24aae45
Nguyen Ngoc Anh Tuan <64023863+Tuan2002@users.noreply.github.com>
2024-05-26 02:19:57 +0700

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('DB_HOST', 'localhost:3306');
 // DB_USER: Tên đăng nhập MySQL (mặc định là root)
 define('DB_USER', 'root');
 // DB_PASSWORD: Mật khẩu đăng nhập MySQL (mặc định rỗng)
-define('DB_PASSWORD', 'vinhuni@123');
+define('DB_PASSWORD', '');
 // DB_NAME: Tên cơ sở dữ liệu sẽ sử dụng
 define('DB_NAME', 'todo');
 // DB_PORT: Cổng kết nối tới cơ sở dữ liệu (mặc định là 3306)
